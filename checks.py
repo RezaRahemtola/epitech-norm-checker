e8eb1eabd7f285f7727fa5d6c0bec01bb77df8cd
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-02-15 16:48:40 +0100

    [ADD] Output colored

--- checks.py
+++ checks.py
@@ -2,15 +2,15 @@ import re
 
 
 def line_break_end_of_file(filename: str, content: str) -> list:
-    name: str = "A3: Line break at the end of file"
-    line: int = content.count('\n') + 1
+    name = "A3: Line break at the end of file"
+    line = content.count('\n') + 1
 
     if not content.endswith('\n'):
         return [{"type": "info", "file": filename, "line": line, "name": name}]
     return []
 
 def function_separation(filename: str, content: str) -> list:
-    name: str = "G2: Separation of functions"
+    name = "G2: Separation of functions"
 
     if len(re.findall("}\n{3,}", content)) > 0:
         line: int = content.split("\n\n\n")[0].count('\n') + 2
@@ -18,7 +18,7 @@ def function_separation(filename: str, content: str) -> list:
     return []
 
 def leading_line(filename: str, content: str) -> list:
-    name: str = "G9 - Leading/Trailing lines"
+    name = "G9 - Leading/Trailing lines"
 
     if content.startswith('\n'):
         return [{"type": "minor", "file": filename, "line": 1, "name": name}]
