cc918992238e7e6b7d6342909e935a3699e481d6
Reza Rahemtola <reza.rahemtola@epitech.eu>
2022-02-15 13:49:25 +0100

    [ADD] A3 check function

--- checks.py
+++ checks.py
@@ -1,8 +1,18 @@
 import re
 
 
+def line_break_end_of_file(filename: str, content: str) -> list:
+    name: str = "A3: Line break at the end of file"
+    line: int = content.count('\n') + 1
+
+    if not content.endswith('\n'):
+        return [{"type": "info", "file": filename, "line": line, "name": name}]
+    return []
+
 def function_separation(filename: str, content: str) -> list:
+    name: str = "G2: Separation of functions"
+
     if len(re.findall("}\n{3,}", content)) > 0:
-        index: int = content.split("\n\n\n")[0].count('\n') + 2
-        return [{"type": "minor", "file": filename, "line": index, "name": "G2: Separation of functions"}]
+        line: int = content.split("\n\n\n")[0].count('\n') + 2
+        return [{"type": "minor", "file": filename, "line": line, "name": name}]
     return []
